35ef1f81d160715278aa99aec7608341fb6a751b
Paul Schuberth <mail@paulschuberth.de>
2021-04-25 20:51:40 +0200

    fix git diff styling

--- setup.sh
+++ setup.sh
@@ -3,22 +3,11 @@
 # load submodules
 git submodule update --init --recursive
 
-
 # Environment setup
 script_dir=$(pwd)
 
 # load vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-#link_to_home "vim/" "~/.vim/"
-#link_to_home "alacritty.yml" "~/.config/alacritty/alacritty.yml"
-#link_to_home "tmux/tmux.conf" "~/.tmux.conf"
-#link_to_home "karabiner.json" "~/.config/karabiner/karabiner.json"
-#link_to_home "fish/" "~/.config"
-#link_to_home "starship.toml" "~/.config/starship.toml"
-#link_to_home ".gitconfig" 
-#link_to_home "bat/" "~/.config"
 
 original="$script_dir"/"$1"
 basename=$(basename $original)
