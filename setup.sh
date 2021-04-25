5860187578dc65ab12e18874a119fcb3dad9a32a
Paul Schuberth <mail@paulschuberth.de>
2021-04-25 21:22:02 +0200

    restore deleted vim directory

--- setup.sh
+++ setup.sh
@@ -3,21 +3,39 @@
 # load submodules
 git submodule update --init --recursive
 
+# load vim.plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Environment setup
 script_dir=$(pwd)
 
-# load vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-original="$script_dir"/"$1"
-basename=$(basename $original)
-
+rm ~/.config/bat
 ln -sF ~/dotfiles/bat/ ~/.config/bat
+
+rm ~/.config/alacritty
 ln -sF ~/dotfiles/alacritty/ ~/.config/alacritty
+
+rm -rf ~/.tmux.conf
 ln -sF ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+rm ~/.config/karabiner
 ln -sF ~/dotfiles/karabiner/ ~/.config/karabiner
+
+rm ~/.config/fish
 ln -sF ~/dotfiles/fish/ ~/.config/fish
+
+rm ~/.config/starship.toml
 ln -sF ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+
+rm ~/.gitconfig
 ln -sF ~/dotfiles/.gitconfig ~/.gitconfig
+
+rm -rf ~/.vim
 ln -sF ~/dotfiles/vim/ ~/.vim
+
+rm ~/.vimrc
 ln -sF ~/dotfiles/vim/vimrc ~/.vimrc
+rm ~/.ideavimrc
+ln -sF ~/dotfiles/vim/vimrc ~/.ideavimrc
+
+
