b14204943838ad6005f3d857c1b5703908ee1648
Paul Schuberth <mail@paulschuberth.de>
2021-04-26 20:15:53 +0200

    update path

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 
 set PATH "$PATH:/Users/schuberth/bin:/Users/schuberth/.sdkman/bin"
+fish_add_path /Users/schuberth/bin
+fish_add_path /Users/schuberth/.sdkman/bin
+fish_add_path /Users/schuberth/.cargo/bin
 
 set fish_greeting
 
