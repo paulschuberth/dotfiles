04b89525d5922c8422cb07952ec9612285bb83d5
Paul Schuberth <mail@paulschuberth.de>
2021-04-30 17:59:37 +0200

    enable editing current command line in editor

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 
 set PATH "$PATH:/Users/schuberth/bin:/Users/schuberth/.sdkman/bin"
+set EDITOR vim
+
 fish_add_path /Users/schuberth/bin
 fish_add_path /Users/schuberth/.sdkman/bin
 fish_add_path /Users/schuberth/.cargo/bin
