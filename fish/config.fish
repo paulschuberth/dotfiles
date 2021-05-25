5e1071f1faea01d19e40977d89ab9c13500db63a
Paul Schuberth <mail@paulschuberth.de>
2021-05-25 08:15:09 +0200

    add nvm.fish

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,7 @@ fish_add_path /Users/schuberth/.cargo/bin
 set fish_greeting
 
 starship init fish | source
+
+set NVM_DIR "$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
