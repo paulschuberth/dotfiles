b6b16ce1fd98e70b3d1be65b4323ca41c1642599
Paul Schuberth <mail@paulschuberth.de>
2021-10-11 22:52:29 +0200

    change bat theme when changing fish theme

--- fish/functions/change_background.fish
+++ fish/functions/change_background.fish
@@ -47,4 +47,13 @@ function change_background --argument mode_setting
     case light
       alacritty-theme gruvbox_light
   end
+
+  # change bat theme
+  switch $mode
+    case dark
+      sed -i .backup 's/gruvbox-light/gruvbox-dark/g' ~/.config/bat/config
+    case light
+      sed -i .backup 's/gruvbox-dark/gruvbox-light/g' ~/.config/bat/config
+  end
+  rm ~/.config/bat/config.backup
 end
