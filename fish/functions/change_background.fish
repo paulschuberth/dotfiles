87dccc2b167af98fcf6f879efbdc5c053dc614d3
Paul Schuberth <mail@paulschuberth.de>
2022-07-06 23:04:22 +0200

    apparently mostly whitespace changed

--- fish/functions/change_background.fish
+++ fish/functions/change_background.fish
@@ -2,27 +2,28 @@ function change_background --argument mode_setting
   # change background to the given mode. If mode is missing, 
   # we try to deduct it from the system settings.
 
-  set -l mode "light" # default value
+  set -f mode "light" # default value
   if test -z $mode_setting
-    set -l val (defaults read -g AppleInterfaceStyle) >/dev/null
-    if test $status -eq 0
-      set mode "dark"
+    if test output (defaults read -g AppleInterfaceStyle 2> /dev/null) = "Dark"
+      set -f mode "light"
+    else
+      set -f mode "dark"
     end
   else
     switch $mode_setting
       case light
-        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false" >/dev/null
-        set mode "light"
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false" &>/dev/null
+        set -f mode "light"
       case dark
-        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true" >/dev/null
-        set mode "dark"
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true" &>/dev/null
+        set -f mode "dark"
     end
   end
 
-  # change alacritty
+# change alacritty
   switch $mode
     case dark
-        alacritty-themes $ALACRITTY_THEME_DARK
+       alacritty-themes $ALACRITTY_THEME_DARK
     case light
         alacritty-themes $ALACRITTY_THEME_LIGHT
   end
