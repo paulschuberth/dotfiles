ea76e50d7ffe1d992c5d421c148417d643c93fa9
Paul Schuberth <mail@paulschuberth.de>
2023-05-27 11:36:45 +0200

    Remove function settings env variables

--- fish/functions/azure-resource-group.fish
+++ fish/functions/azure-resource-group.fish
@@ -2,9 +2,11 @@
 function aint
     set -gx SP_CLIENT_ID $AZ_CLIENT_ID_ACPP_INT
     set -gx SP_CLIENT_SECRET $AZ_CLIENT_SECRET_ACPP_INT
+    az account set --subscription CARIAD.mega.westeurope-product-dev-berlin
 end
 
 function aprod
     set -gx SP_CLIENT_ID $AZ_CLIENT_ID_ACPP_PROD
     set -gx SP_CLIENT_SECRET $AZ_CLIENT_SECRET_ACPP_PROD
+    az account set --subscription CARIAD.mega.westeurope-product-prod-lisbon
 end
