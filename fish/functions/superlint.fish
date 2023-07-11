5acde2af765e7119e7c48440e5cfeaa29f4ac08f
Paul Schuberth <mail@paulschuberth.de>
2023-07-11 23:40:34 +0200

    feat: fish function for super-linter

--- fish/functions/superlint.fish
+++ fish/functions/superlint.fish
@@ -3,7 +3,12 @@
 function superlint
 
     docker pull github/super-linter:latest --platform linux/arm64
-    docker run -e RUN_LOCAL=true -e USE_FIND_ALGORITHM=true -e VALIDATE_GITLEAKS=false -v (pwd):/tmp/lint github/super-linter
+    docker run \
+        -e RUN_LOCAL=true \
+        -e USE_FIND_ALGORITHM=true \
+        -e VALIDATE_GITLEAKS=false \
+        -e VALIDATE_CSS=false \
+        -v (pwd):/tmp/lint github/super-linter
 
 
 end
